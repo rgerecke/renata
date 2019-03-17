022518f28b67e69d2f5e9ef6dd9d89580ee70e8a
rvg02010 <renatavgerecke@gmail.com>
2019-03-16 21:22:09 -0400

    add get_var_labels and get_val_labels functions

--- R/get_dictionary-function.R
+++ R/get_dictionary-function.R
@@ -21,3 +21,47 @@ get_dictionary <- function(data) {
     tidyr::unnest() %>%
     tidyr::complete(variable, class)
 }
+
+
+#' Create table of variable labels
+#'
+#' `get_var_labels` creates a data frame of variable names and labels
+#'
+#' This function takes a data frame created using `haven::read_sav` and
+#' returns a data frame with the SPSS variable labels for each variable
+#'
+#' @param data A data frame or tibble created using `haven::read_sav`
+#' @return A data frame with the `variable` names and respective SPSS `label`
+#' @keywords SPSS haven sav
+#' @export
+#'
+
+
+get_var_labels <- function(data) {
+  if (purrr::some(data, haven::is.labelled)==FALSE) {stop("Not a labelled data set")}
+  purrr::map_chr(data, ~attr(., "label")) %>%
+    tibble::enframe(name = "variable", value ="label")
+}
+
+#' Create table of value labels
+#'
+#' `get_val_labels` creates a data frame of variable names and labels
+#'
+#' This function takes a data frame created using `haven::read_sav` and
+#' returns a data frame with the SPSS labelled values for each variable
+#'
+#' @param data A data frame or tibble created using `haven::read_sav`
+#' @return A data frame with the `variable`, `value` (which has labels), and respective SPSS `value_name`
+#' @keywords SPSS haven sav
+#' @export
+#'
+
+get_val_labels <- function(data) {
+  if (purrr::some(data, haven::is.labelled)==FALSE) {stop("Not a labelled data set")}
+  purrr::map(data, ~attr(., "labels")) %>%
+    purrr::compact() %>%
+    tibble::enframe(name = "variable") %>%
+    dplyr::mutate(value_name = map(value, names)) %>%
+    tidyr::unnest()
+}
+
